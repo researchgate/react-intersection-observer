9aa1e11b9f807f8f2d90799588ea67631625ffdb
tomasz stryjewski <stryjewski.t@gmail.com>
2017-11-24 19:16:19 +0000

    tweak `parseRootMargin`
    this change achieves few things:
    1. provides default margin value, if omitted, instead of when falsy
    2. type-checks `rootMargin`
    3. trims the incoming stream first, to handle strings like ` 0px` properly
    4. caches the `RegExp` outside of function scope, not to create on every function call
    5. uses destrucuting with default value to "simplify" handling shortcut

--- src/utils.js
+++ src/utils.js
@@ -4,22 +4,22 @@ export function isDOMTypeElement(element) {
     return React.isValidElement(element) && typeof element.type === 'string';
 }
 
-export function parseRootMargin(rootMargin) {
-    const marginString = rootMargin || '0px';
-    const re = /^-?\d*\.?\d+(px|%)$/;
-    const margins = marginString.split(/\s+/).map(margin => {
-        if (!re.test(margin)) {
+const marginRE = /^-?\d*\.?\d+(px|%)$/;
+
+export function parseRootMargin(rootMargin = '0px') {
+    if (typeof rootMargin !== 'string') {
+        throw new Error('rootMargin must be a String');
+    }
+    
+    // Handles shorthand.
+    const [m0, m1 = m0, m2 = m0, m3 = m0] = marginString.trim().split(/\s+/).map(margin => {
+        if (!marginRE.test(margin)) {
             throw new Error('rootMargin must be specified in pixels or percent');
         }
         return margin;
     });
 
-    // Handles shorthand.
-    margins[1] = margins[1] || margins[0];
-    margins[2] = margins[2] || margins[0];
-    margins[3] = margins[3] || margins[1];
-
-    return margins.join(' ');
+    return `${m0} ${m1} ${m2} ${m3}`;
 }
 
 export function shallowCompareOptions(next, prev) {
