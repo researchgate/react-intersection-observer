83896be64709fea0c13b8f3623280cb2023a94b6
Luis Merino <mail@luismerino.name>
2019-04-18 18:13:58 +0200

    chore(errorReporter): added configurable error reporter

---
+++ src/__tests__/config.spec.js
@@ -0,0 +1,33 @@
+/* eslint-env jest */
+import Config from '../config';
+import invariant from 'invariant';
+
+jest.mock('invariant', () => jest.fn());
+
+describe('Config', () => {
+    const testErrorReporter = jest.fn();
+    const errorMsg = 'Intentionally throw exception';
+
+    test('default errorReporter', () => {
+        Config.errorReporter(errorMsg);
+        expect(invariant).toBeCalledWith(false, errorMsg);
+    });
+
+    test('custom errorReporter', () => {
+        const defaultTestErrorReporter = Config.errorReporter;
+        Config.errorReporter = testErrorReporter;
+
+        Config.errorReporter(errorMsg);
+        expect(testErrorReporter).toBeCalledWith(errorMsg);
+
+        Config.errorReporter = defaultTestErrorReporter;
+    });
+
+    test('custom non-callable errorReporter', () => {
+        expect(() => {
+            Config.errorReporter = 'fail';
+        }).toThrowErrorMatchingInlineSnapshot(
+            `"ReactIntersectionObserver: \`Config.errorReporter\` must be a callable"`,
+        );
+    });
+});
